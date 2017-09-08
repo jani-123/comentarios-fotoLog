ef6f30f0b931b045182ea94221ee293dd90a038e
jani-123 <janina.rqa@gmail.com>
2017-09-08 13:29:31 -0500

    corrigiendo clear6

--- js/main.js
+++ js/main.js
@@ -8,18 +8,19 @@ function guardarDatos()
 	{
 		var clave=localStorage.key(i);
 		var value=localStorage.getItem(clave);
-		var divPrincipal = document.getElementById('addComentario');
-		var divComentario = document.createElement('div');
-  	        divComentario.setAttribute('class','divComentario');
-  	    var name = document.createElement('p');
-  	    var name1 = document.createTextNode(clave);
-  	    var comenta = document.createElement('p');
-  	    var comenta1 = document.createTextNode(value);
-  	    comenta.appendChild(comenta1);
-	    name.appendChild(name1);
-	    divComentario.appendChild(name);
-	    divComentario.appendChild(comenta);
-	    divPrincipal.appendChild(divComentario);
+		// var divPrincipal = document.getElementById('addComentario');
+		$("#addComentario").append('<div><p>'+clave+'</p><p>'+value+'</p></div>')
+		// var divComentario = document.createElement('div');
+  // 	        divComentario.setAttribute('class','divComentario');
+  // 	    var name = document.createElement('p');
+  // 	    var name1 = document.createTextNode(clave);
+  // 	    var comenta = document.createElement('p');
+  // 	    var comenta1 = document.createTextNode(value);
+  // 	    comenta.appendChild(comenta1);
+	 //    name.appendChild(name1);
+	 //    divComentario.appendChild(name);
+	 //    divComentario.appendChild(comenta);
+	 //    divPrincipal.appendChild(divComentario);
 
 	}
 
@@ -27,6 +28,7 @@ function guardarDatos()
 function limpiar()
 {
 	localStorage.clear();// limpia
-	
+	$("#addComentario").empty();
+
 	
 }
